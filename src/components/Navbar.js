5e6d159f319711a61629e174cc0b136d54a59759
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-10-31 18:46:25 +0300

    Update Navbar menu names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
     <div className='navbar'>
         <div className='navbar-menus'>
           <Link to="/" style={{textDecoration: 'none', color:'black'}}> HOME </Link>
-          <Link to="/manageprojects" style={{textDecoration: 'none', color:'black'}}> MANAGE PROJECTS </Link>
+          <Link to="/managetasks" style={{textDecoration: 'none', color:'black'}}> MANAGE TASKS </Link>
           <Link to="/eventcalendar" style={{textDecoration: 'none', color:'black'}}> EVENT CALENDAR </Link>
-          <Link to="/projectsummary" style={{textDecoration: 'none', color:'black'}}> PROJECT SUMMARY </Link>
+          <Link to="/tasksummary" style={{textDecoration: 'none', color:'black'}}> TASK SUMMARY </Link>
           <Link to="/login" style={{textDecoration: 'none', color:'black'}}> LOGOUT </Link>
         </div>
     </div>
