a87a3bc99e274971ddacf05d748cdc2beac839d4
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-10-31 18:48:29 +0300

    Update redirect links

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 function Signup() {
   return (
     <div className='signup'>
-        <Link to="Login" style={{textDecoration: 'none', color:'black'}} className='reg-back'> <BiArrowBack size={'1.8em'}/> </Link>
+        <Link to="/signup" style={{textDecoration: 'none', color:'black'}} className='reg-back'> <BiArrowBack size={'1.8em'}/> </Link>
         <h1 className='h1-signup'>Create Account</h1>
         <div className='signup-form'>
             <Form>
@@ -19,14 +19,9 @@ function Signup() {
                 <Form.Group className="mb-4" controlId="formBasicPassword">
                     <Form.Control type="password" placeholder="Confirm Password" />
                 </Form.Group>
-                <Form.Text className='password-text' id="passwordHelpBlock" muted>
-                    Your password must be 8-20 characters long,  contain letters and <br/>
-                    numbers, and must not contain spaces, special characters, or emoji.
-                </Form.Text>
-            
             </Form>
         </div>
-        <Button className="mt-5" variant="primary" type="submit">Register</Button>
+        <Button className="mt-5" variant="primary" type="submit" href="./login">Register</Button>
 
     </div>
   )
