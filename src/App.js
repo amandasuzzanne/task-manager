0359609da85bde774426ad2967e720b830f7aec2
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-10-31 21:22:18 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from "./pages/Home"
+import ManageTasks from './pages/ManageTasks';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <header className="App-header" />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="managetasks" element={<ManageTasks/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="signup" element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
